a23c43b8dc66bb08f12d0db87864daad7a2aa83f
Jason R. Coombs <jaraco@jaraco.com>
2015-06-16 18:30:48 -0400

    wclip.get_html returns a Snippet object, resolve the HTML attribute.

--- jaraco/clipboard/Windows.py
+++ jaraco/clipboard/Windows.py
@@ -4,6 +4,8 @@ copy_text = wclip.set_unicode_text
 paste_text = wclip.get_unicode_text
 
 copy_html = wclip.set_html
-paste_html = wclip.get_html
+
+def paste_html():
+	wclip.get_html().html
 
 paste_image = wclip.get_image
