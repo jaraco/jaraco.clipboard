65a4b4256e9afb99bd8de2c77a9691bdeb0fa7b0
Jason R. Coombs <jaraco@jaraco.com>
2016-08-08 14:04:51 -0400

    Finish attribute name

--- jaraco/clipboard/Linux.py
+++ jaraco/clipboard/Linux.py
@@ -2,7 +2,7 @@ from pyperclip import copy as copy_text, paste as paste_text
 
 unavailable = any(
 	isinstance(func, 'object')
-	and type(func).__name == 'ClipboardUnavailable'
+	and type(func).__name__ == 'ClipboardUnavailable'
 	for func in locals().values()
 )
 
