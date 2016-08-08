10f95dfe356989e2f2afb3840ed3973890d8dcd0
Jason R. Coombs <jaraco@jaraco.com>
2016-08-08 13:34:04 -0400

    Add two trivial tests

---
+++ test_all.py
@@ -0,0 +1,23 @@
+import pytest
+
+from jaraco import clipboard
+
+
+def needs_command(name):
+	cmd = vars(clipboard)[name]
+
+	return pytest.mark.xfail(cmd is clipboard._not_implemented,
+		reason="{name} not implemented on this platform",
+	)
+
+
+@needs_command("copy_text")
+def test_copy_text():
+	clipboard.copy_text("foo")
+
+
+@needs_command("paste_text")
+def test_paste_text():
+	# first ensure there's something on the clipboard:
+	clipboard.copy_text('test for paste')
+	assert clipboard.paste_text() == 'test for paste'
