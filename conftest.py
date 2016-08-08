256407be4bdc741c40e3d32a66904d5ff973f13f
Jason R. Coombs <jaraco@jaraco.com>
2016-08-08 13:07:05 -0400

    Ignore other platform modules when running tests.

---
+++ conftest.py
@@ -0,0 +1,9 @@
+import platform
+
+platform_names = 'Linux', 'Darwin', 'Windows'
+
+collect_ignore = [
+	'jaraco/clipboard/{name}.py'.format(**locals())
+	for name in platform_names
+	if platform.system() != name
+]
