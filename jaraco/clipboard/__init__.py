bb6468268beaac63ce513f764ec435a338e04986
Jason R. Coombs <jaraco@jaraco.com>
2018-10-24 20:56:54 -0400

    Feed the hobgoblins (delint).

--- jaraco/clipboard/__init__.py
+++ jaraco/clipboard/__init__.py
@@ -10,15 +10,18 @@ _modes = 'copy', 'paste'
 _formats = 'text', 'html', 'image'
 _methods = map('_'.join, itertools.product(_modes, _formats))
 
+
 def _not_implemented(*args, **kwargs):
 	raise NotImplementedError("format not supported")
+
 
 for name in _methods:
 	func = getattr(_platform_mod, name, _not_implemented)
 	globals().update({name: func})
 
-copy = copy_text
-paste = paste_text
+
+copy = copy_text  # noqa: F821
+paste = paste_text  # noqa: F821
 
 __all__ = [
 	name for name, func in globals().items()
