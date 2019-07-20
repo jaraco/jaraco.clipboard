fd3585cf974148aece9430b354687f14c36f8a2e
Jason R. Coombs <jaraco@jaraco.com>
2019-07-20 18:41:18 -0400

    In Darwin.copy_html, translate first parameter to html keyword argument. Fixes #4.

--- jaraco/clipboard/Darwin.py
+++ jaraco/clipboard/Darwin.py
@@ -6,4 +6,7 @@ paste_text = richxerox.paste
 copy_text = richxerox.copy
 
 paste_html = functools.partial(richxerox.paste, format='html')
-copy_html = functools.partial(richxerox.copy, format='html')
+
+
+def copy_html(content):
+    return richxerox.copy(html=content)
