c2d365d285342078192e4d225bff87ca41a51e92
Jason R. Coombs <jaraco@jaraco.com>
2023-06-05 22:17:05 -0400

    Ran pyupgrade for Python 3.7+.

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ import platform
 platform_names = 'Linux', 'Darwin', 'Windows'
 
 collect_ignore = [
-    'jaraco/clipboard/{name}.py'.format(**locals())
+    f'jaraco/clipboard/{name}.py'
     for name in platform_names
     if platform.system() != name
 ]
